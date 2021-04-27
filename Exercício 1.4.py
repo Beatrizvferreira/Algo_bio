dd8f31c5963720d396119a79ec80d15371a67d07
Beatrizvferreira <beeatrizvferreira@gmail.com>
2021-04-27 16:05:09 -0300

    Lista 1

--- Exercício 1.4.py
+++ Exercício 1.4.py
@@ -1,9 +1,9 @@
 import math
 
-print(f'Quantos cigarros você fuma por dia e por quantos anos?')
-qnt, anos = input().split(' ')
-qnt = int(qnt)
-anos = int(anos)
+print(f'Quantos cigarros você fuma por dia?')
+qnt = int(input())
+print(f'Faz quantos anos que você fuma?')
+anos = int(input())
 vida_perdida = qnt*10*30*365 #Quantidade de cigarros fumados em um anos
 vida_perdida *= anos #Quantidade de cigarros fumado em n anos
 
---
+++ Exercício 1.5.py
@@ -0,0 +1,12 @@
+print('Quantos dias?')
+dias = int(input())
+print('Quantas horas?')
+horas = int(input())
+print('Quantos minutos?')
+minutos = int(input())
+print('Quantos segundos?')
+segundos = int(input())
+
+soma = segundos + 60*minutos + 60*60*horas + 60*60*24*dias
+
+print(f'O total em segundos é {soma}')
